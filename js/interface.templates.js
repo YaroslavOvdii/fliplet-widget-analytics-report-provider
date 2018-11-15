beb4f55f52eaa1f3b27ed8f066f31d2bec12fb21
Nicholas Valbusa <squallstar@gmail.com>
2018-11-15 19:16:15 +0100

    updated templates

--- js/interface.templates.js
+++ js/interface.templates.js
@@ -3,7 +3,7 @@ this["Fliplet"]["Widget"] = this["Fliplet"]["Widget"] || {};
 this["Fliplet"]["Widget"]["Templates"] = this["Fliplet"]["Widget"]["Templates"] || {};
 
 this["Fliplet"]["Widget"]["Templates"]["templates.interface.active-user"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "{{#each this}}\n  <div class=\"analytics-row\">\n    <div class=\"analytics-row-name\">\n      <p>{{ _userEmail }}</p>\n    </div>\n    <div class=\"analytics-row-score\">\n      <p>{{#if sessionsCount}}{{sessionsCount}}{{else}}{{ count }}{{/if}}</p>\n    </div>\n  </div>\n{{/each}}";
+    return "{{#each this}}\n  <div class=\"analytics-row\">\n    <div class=\"analytics-row-name\">\n      <p>{{userEmail}}</p>\n    </div>\n    <div class=\"analytics-row-score\">\n      <p>{{ count }}</p>\n    </div>\n  </div>\n{{/each}}";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.interface.app-metrics"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
